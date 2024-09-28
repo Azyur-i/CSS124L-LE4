fac225cf944b70f39fdbda1d79c5b9113e98fca0
XenDQ <xeanxyrone@gmail.com>
2024-09-28 22:58:56 +0800

    pom.xml file cant read the entire file, had to redo

--- le4/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.groupfour {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-
-    opens com.groupfour to javafx.fxml;
-    exports com.groupfour;
-}

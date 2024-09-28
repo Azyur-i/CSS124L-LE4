5eb736f222548c41c148baf3629e96ee9153e068
XenDQ <xeanxyrone@gmail.com>
2024-09-29 00:55:27 +0800

    quickly added button shit. pls take over on adding individual entry functionalities

--- le4/src/main/java/com/groupfour/App.java
+++ le4/src/main/java/com/groupfour/App.java
@@ -4,17 +4,14 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Separator;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import com.groupfour.JsonReader;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
-import java.io.File;
 
 public class App extends Application {
 
@@ -35,7 +32,9 @@ public class App extends Application {
         //UI Elements
         VBox sideBar = new VBox();
         VBox mainVBox = new VBox();
-        VBox firstCtnBox = new VBox();
+        VBox gamesVBox = new VBox();
+        VBox appsVBox = new VBox();
+        VBox moviesVBox = new VBox();
         Label label = new Label("PLACEHOLDER");
 
         label.getStyleClass().add("placeholder-label");
@@ -45,34 +44,55 @@ public class App extends Application {
         Button appBtn = new Button("Apps");
         Button movieBtn = new Button("Movies");
 
+        //Button triggers
+        gameBtn.setOnAction(e -> {
+            mainVBox.getChildren().clear();
+            mainVBox.getChildren().add(gamesVBox);
+        });
+
+        appBtn.setOnAction(e -> {
+            mainVBox.getChildren().clear();
+            mainVBox.getChildren().add(appsVBox);
+        });
+
+        movieBtn.setOnAction(e -> {
+            mainVBox.getChildren().clear();
+            mainVBox.getChildren().add(moviesVBox);
+        });
+
+        //Add Json components
         JsonNode node = new ObjectMapper().readTree(getClass().getResourceAsStream("appData.json"));
         if (node.isArray()) {
+            int count = 0;
             for (JsonNode element : node) {
                 String title = element.get("title").asText();
                 String dev = element.get("dev").asText();
                 double starRating = element.get("star_rating").asDouble();
                 String downloads = element.get("downloads").asText();
 
-                Label gameLabel = new Label(title);
+                Label titleLabel = new Label(title);
                 Label devLabel = new Label(dev);
                 Label rateLabel = new Label(String.valueOf(starRating));
                 Label dlLabel = new Label(downloads);
-                firstCtnBox.getChildren().addAll(gameLabel, devLabel, rateLabel, dlLabel);
 
+                if (count < 4) {
+                    gamesVBox.getChildren().addAll(titleLabel, devLabel, rateLabel, dlLabel);
+                } else if (count < 8) {
+                    appsVBox.getChildren().addAll(titleLabel, devLabel, rateLabel, dlLabel);
+                } else {
+                    moviesVBox.getChildren().addAll(titleLabel, devLabel, rateLabel, dlLabel);
+                }
+                count++;
             }
         } else {
             System.out.println("Error: JSON data is not an array");
         }
         
-        gameBtn.getStyleClass().add("button");
-        appBtn.getStyleClass().add("button");
-        movieBtn.getStyleClass().add("button");
-        
         //Add UI elements
         sideBar.getChildren().addAll(label, gameBtn, appBtn, movieBtn);
         root.getChildren().addAll(sideBar, mainVBox);
 
-        mainVBox.getChildren().addAll(firstCtnBox);
+        mainVBox.getChildren().add(gamesVBox);
         
         
     }

f17d840e4060b1c3a82429ac16e86d1bbab8574c
Centhor <vbhizon@mymail.mapua.edu.ph>
2024-09-29 14:12:34 +0800

    Changed how database is stored, added "description", made extended view scene

--- le4/src/main/java/com/groupfour/App.java
+++ le4/src/main/java/com/groupfour/App.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -16,6 +18,7 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    private GridPane selectedPane;
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -61,7 +64,23 @@ public class App extends Application {
         });
 
         //Add Json components
-        JsonNode node = new ObjectMapper().readTree(getClass().getResourceAsStream("appData.json"));
+        JsonNode appNode = new ObjectMapper().readTree(getClass().getResourceAsStream("appData.json"));
+        JsonNode gameNode = new ObjectMapper().readTree(getClass().getResourceAsStream("gameData.json"));
+        JsonNode movieNode = new ObjectMapper().readTree(getClass().getResourceAsStream("movieData.json"));
+        
+        //Populate VBox containers
+        populateVbox(appNode,appsVBox, stage);
+        populateVbox(gameNode,gamesVBox, stage);
+        populateVbox(movieNode, moviesVBox, stage);
+        
+        //Add UI elements
+        sideBar.getChildren().addAll(label, gameBtn, appBtn, movieBtn);
+        root.getChildren().addAll(sideBar, mainVBox);
+
+        mainVBox.getChildren().add(gamesVBox);
+    }
+
+    public void populateVbox(JsonNode node, VBox vbox,Stage stage){
         if (node.isArray()) {
             int count = 0;
             for (JsonNode element : node) {
@@ -69,32 +88,75 @@ public class App extends Application {
                 String dev = element.get("dev").asText();
                 double starRating = element.get("star_rating").asDouble();
                 String downloads = element.get("downloads").asText();
+                String description = element.get("description").asText();
 
+                GridPane infoPane = new GridPane();
+
+                //Main Page Info
                 Label titleLabel = new Label(title);
                 Label devLabel = new Label(dev);
                 Label rateLabel = new Label(String.valueOf(starRating));
                 Label dlLabel = new Label(downloads);
 
+                //Copy for Extended Details Page (.add consumes the variable, need two of these- one for sidebar one for details)
+                Label titleCopy = new Label(title);
+                Label devCopy = new Label(dev);
+                Label rateCopy = new Label(String.valueOf(starRating));
+                Label dlCopy = new Label(downloads);
+                Label descLabel = new Label(description);
+                
+                //Spacer
+                infoPane.add(new Label(" "), 0, 0);
+                infoPane.add(new Label(" "), 1, 0);
+
+                //Contents
+                infoPane.add(titleLabel, 0, 1);           
+                infoPane.add(devLabel, 0, 2);
+                infoPane.add(rateLabel, 0, 3);
+                infoPane.add(dlLabel, 0, 4);
+                
+                //Click event on info,  opens extended details page
+                infoPane.setOnMouseClicked(event -> {
+
+                    //First instance of this will error
+                    try{
+                    selectedPane.setStyle("-fx-background-color:#808080;"); //CSS Styling, 
+                    } catch (Exception e){
+                        System.out.println("First CLick");
+                    }
+
+
+                    selectedPane = infoPane;
+                    AnchorPane container = new AnchorPane();
+                    HBox parentHBox = new HBox();
+                    VBox infoVBox = new VBox();
+                    
+                    vbox.setStyle("-fx-background-color:#808080;"); //CSS Styling
+                    selectedPane.setStyle("-fx-background-color: #C0C0C0;"); //CSS Styling
+
+                    //**CSS Styling - Remove these at the end, just showed the variables you need to use
+
+                    infoVBox.getChildren().addAll(titleCopy, devCopy, rateCopy, dlCopy,descLabel);
+                    parentHBox.getChildren().addAll(vbox,infoVBox);
+                    container.getChildren().add(parentHBox);
+                    Scene infoScene = new Scene(container, 1280, 720);
+                    stage.setScene(infoScene);
+                    stage.show();
+
+
+
+                });
+
                 if (count < 4) {
-                    gamesVBox.getChildren().addAll(titleLabel, devLabel, rateLabel, dlLabel);
-                } else if (count < 8) {
-                    appsVBox.getChildren().addAll(titleLabel, devLabel, rateLabel, dlLabel);
-                } else {
-                    moviesVBox.getChildren().addAll(titleLabel, devLabel, rateLabel, dlLabel);
-                }
+
+                    vbox.getChildren().addAll(infoPane);
+                    count=-1;
+                } 
                 count++;
             }
         } else {
             System.out.println("Error: JSON data is not an array");
         }
-        
-        //Add UI elements
-        sideBar.getChildren().addAll(label, gameBtn, appBtn, movieBtn);
-        root.getChildren().addAll(sideBar, mainVBox);
-
-        mainVBox.getChildren().add(gamesVBox);
-        
-        
     }
 
     public static void main(String[] args) {
--- le4/src/main/java/module-info.java
+++ le4/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.groupfour {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.fasterxml.jackson.databind;
+    requires javafx.graphics;
 
     opens com.groupfour to javafx.fxml;
     exports com.groupfour;
